833ce9f19cb2c5aa98fa0f104328feedfbff1592
Tobias Reize <reizeto@web.de>
2025-02-07 11:34:56 +0100

    added routes and startpage component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { StartpageComponent } from './features/startpage/startpage.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: StartpageComponent},
+    // { path: 'sign-up', },
+    // { path: 'login', },
+    // { path: 'forgot-password', },
+    // { path: 'reset-password', },
+    // { path: 'video-offer', },
+    // { path: 'imprint', },
+    // { path: 'privacy-policy', },
+];
