e727dd4512edaecaa4e12f9f17e0a3a08c9f9a60
Tobias Reize <reizeto@web.de>
2025-02-11 10:16:33 +0100

    updated startpage, added new colors and updated button and input design

--- src/app/features/startpage/startpage.component.ts
+++ src/app/features/startpage/startpage.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-startpage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './startpage.component.html',
   styleUrl: './startpage.component.scss'
 })
 export class StartpageComponent {
+
+  signUpEmail: string = '';
+
+  async onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid) {
+      // tbd.
+      console.log('signUpEmail:', this.signUpEmail);
+      ngForm.resetForm();
+    }
+  }
 
 }
