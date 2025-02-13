d52d824b5f1df170edf55974ab783333bd03070b
Tobias Reize <reizeto@web.de>
2025-02-13 14:18:56 +0100

    updated login form and input style

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, HeaderComponent, FooterComponent],
+  imports: [FormsModule, RouterLink, HeaderComponent, FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginData = {
+    email: '',
+    password: ''
+  }
+  isPasswordVisible: boolean = false;
+
+
+  showPassword(state:boolean) {
+    if (state == true) {
+      this.isPasswordVisible = true;
+    } else {
+      this.isPasswordVisible = false;
+    }
+  }
 
 
   onSubmit(ngForm: NgForm) {
       if (ngForm.submitted && ngForm.form.valid) {
         // tbd.
         console.log('Erfolgreich!');
+        ngForm.resetForm();
       } else {
         console.log('Fehler!!!');
       }
