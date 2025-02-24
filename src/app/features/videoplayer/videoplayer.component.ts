5b9b85e4ce7f1dd6aaa6fafd43ebf82000d78f7f
Tobias Reize <reizeto@web.de>
2025-02-24 10:17:24 +0100

    added header to videoplayer

--- src/app/features/videoplayer/videoplayer.component.ts
+++ src/app/features/videoplayer/videoplayer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-videoplayer',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './videoplayer.component.html',
   styleUrl: './videoplayer.component.scss'
 })
