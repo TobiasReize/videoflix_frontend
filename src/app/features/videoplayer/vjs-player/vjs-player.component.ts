83ff13733d825c9c470fc6891a9203a099345c9e
Tobias Reize <reizeto@web.de>
2025-02-25 16:50:59 +0100

    added some functions to vjs-player controls

--- src/app/features/videoplayer/vjs-player/vjs-player.component.ts
+++ src/app/features/videoplayer/vjs-player/vjs-player.component.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, inject, Input, OnDestroy, OnInit, ViewChild } fr
 import videojs from 'video.js';
 import Player from 'video.js/dist/types/player';
 import { VideoService } from '../../../services/video-service/video.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vjs-player',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vjs-player.component.html',
   styleUrl: './vjs-player.component.scss',
 })
@@ -41,6 +42,45 @@ export class VjsPlayerComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.player) {
       this.player.dispose();
+    }
+  }
+
+
+  togglePlay() {
+    this.player.paused() ? this.player.play() : this.player.pause();
+  }
+
+
+  fastForward() {
+    if (this.player.currentTime()) {
+      const newTime = (this.player.currentTime() ?? 0) + 10;
+      this.player.currentTime(newTime);
+    }
+    console.log('currentTime:', this.player.currentTime());
+  }
+
+
+  rewind() {
+    if (this.player.currentTime()) {
+      const newTime = (this.player.currentTime() ?? 0) - 10;
+      this.player.currentTime(newTime);
+    }
+    console.log('currentTime:', this.player.currentTime());
+  }
+
+
+  toggleMute() {
+    if (this.player) {
+      const isMuted = this.player.muted();
+      this.player.muted(!isMuted);
+    }
+  }
+
+
+  toggleFullscreen() {
+    if (this.player) {
+      const isFullscreen = this.player.isFullscreen();
+      isFullscreen ? this.player.exitFullscreen() : this.player.requestFullscreen();
     }
   }
 
