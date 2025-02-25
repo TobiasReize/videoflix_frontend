caf0ec2827ff6ac1849e294f4d914b4f5daf69ef
Tobias Reize <reizeto@web.de>
2025-02-25 15:19:07 +0100

    added icons, added custom controls to vjs-player component

--- src/app/features/videoplayer/vjs-player/vjs-player.component.ts
+++ src/app/features/videoplayer/vjs-player/vjs-player.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import videojs from 'video.js';
 import Player from 'video.js/dist/types/player';
+import { VideoService } from '../../../services/video-service/video.service';
 
 @Component({
   selector: 'app-vjs-player',
@@ -11,6 +12,7 @@ import Player from 'video.js/dist/types/player';
 })
 export class VjsPlayerComponent implements OnInit, OnDestroy {
 
+  videoService = inject(VideoService);
   player!: Player;
   @ViewChild('target', {static: true}) target!: ElementRef<HTMLVideoElement>;
   @Input() options!: {
