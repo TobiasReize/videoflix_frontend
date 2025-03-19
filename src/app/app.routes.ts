81c053033862b1fde9eaa74e865e1a8e02c2d0e7
Tobias Reize <reizeto@web.de>
2025-03-19 10:09:18 +0100

    added PageNotFoundComponent, updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { ForgotPasswordComponent } from './features/forgot-password/forgot-passw
 import { ResetPasswordComponent } from './features/reset-password/reset-password.component';
 import { VideoOfferComponent } from './features/video-offer/video-offer.component';
 import { VideoplayerComponent } from './features/videoplayer/videoplayer.component';
+import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
 
 
 export const routes: Routes = [
@@ -20,4 +21,5 @@ export const routes: Routes = [
     { path: 'videoplayer', component: VideoplayerComponent },
     { path: 'imprint', component: ImprintComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    { path: '**', component: PageNotFoundComponent },
 ];
